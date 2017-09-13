5d11645107175da50dddae939dbea01fcb928c7b
LoudSilenceRu <loudsilence@list.ru>
2017-09-14 00:47:25 +0300

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", "localhost");
-define("DB", "my_db");
-define("USER", "localhost");
-define("PASS", "");
-
-$connect = mysqli_connect(HOST, USER, PASS, DB) or die("Error: " . mysqli_error($link));
- 
-?>
