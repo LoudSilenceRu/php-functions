9f981e72a4cfb8775df2ae2769785308dd902ee0
LoudSilenceRu <loudsilence@list.ru>
2017-09-14 23:08:56 +0300

    Update db.php

--- db.php
+++ db.php
@@ -4,6 +4,10 @@ define("DB", "my_db");
 define("USER", "localhost");
 define("PASS", "");
 
+//DB Connection
 $connect = mysqli_connect(HOST, USER, PASS, DB) or die("Error " . mysqli_error($connect));
- 
-?>
+
+//Simple sanitize string
+function sanStr($str) {
+	return nl2br(htmlspecialchars(trim($str), ENT_QUOTES), false);
+}
