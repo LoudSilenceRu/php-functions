6b95e13b63279fe05cf16ddbc9b43aa8612f3cb9
LoudSilenceRu <loudsilence@list.ru>
2017-09-15 23:20:19 +0300

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+
+function ajaxOutput($options = null) {
+	die(json_encode($options, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)); //JSON_HEX_TAG
+}
