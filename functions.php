e501902cc44414d5c2f8436cd2aac35d83354a16
LoudSilenceRu <loudsilence@list.ru>
2017-09-16 23:11:11 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,3 +3,15 @@
 function ajaxOutput($options = null) {
 	die(json_encode($options, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)); //JSON_HEX_TAG
 }
+
+function HideEmail($email) {
+	$email = strtolower($email);
+	$explode = explode('@', $email);
+	$pref = "";
+
+	for ($i=0; $i < 3; $i++) { 
+		$pref .= $explode[0][$i];
+	}
+
+	return $pref."***@".$explode[1];
+}
