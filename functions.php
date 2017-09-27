067c0790972265c484df025c50ac8523611b4abe
LoudSilenceRu <loudsilence@list.ru>
2017-09-27 22:33:16 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -53,3 +53,13 @@ function timePassed($timestamp) {
 	else 
 		return plural_form(floor($diff/31536000), array('год','года','лет'));
 }
+
+function curl( $url ) {
+	$ch = curl_init( $url );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false );
+	curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+	$response = curl_exec( $ch );
+	curl_close( $ch );
+	return json_decode($response , true);
+}
